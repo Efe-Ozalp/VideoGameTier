5b03707f2c0808a00859c0a36e6858c33d13ddf6
Ryan Rasavong <ryanrasavong@icloud.com>
2023-10-24 10:54:10 -0500

    initial

--- Video Game Tierlist/Video_Game_TierlistApp.swift
+++ Video Game Tierlist/Video_Game_TierlistApp.swift
@@ -1,17 +1,27 @@
-//
-//  Video_Game_TierlistApp.swift
-//  Video Game Tierlist
-//
-//  Created by Efe Ozalp on 10/24/23.
-//
-
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Video_Game_TierlistApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
