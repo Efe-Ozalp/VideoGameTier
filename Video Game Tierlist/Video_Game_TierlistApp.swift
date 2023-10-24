e64497261bac9effd4377a6863f3cf55fced9bf9
Efe Ozalp <efeozalp0101@gmail.com>
2023-10-24 10:21:55 -0500

    Initial Commit

---
+++ Video Game Tierlist/Video_Game_TierlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Game_TierlistApp.swift
+//  Video Game Tierlist
+//
+//  Created by Efe Ozalp on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Video_Game_TierlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
