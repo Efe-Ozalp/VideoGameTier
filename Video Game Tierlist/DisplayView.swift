db818cee48a513b761d9230cfb0049fb4c3a32c4
Efe Ozalp <efeozalp0101@gmail.com>
2023-11-01 10:17:58 -0500

    yea

--- Video Game Tierlist/DisplayView.swift
+++ Video Game Tierlist/DisplayView.swift
@@ -6,11 +6,58 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+
+struct Game: Identifiable {
+    var id: String // document ID from Firestore
+    var name: String
+    var rank: String
+}
+
 
 struct DisplayView: View {
+    @State private var games: [Game] = []
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                Button("Hello") {
+                    fetchGames()
+                }
+                ForEach(games) { game in
+                    HStack {
+                        Text(game.name)
+                        Spacer()
+                        Text("Tier \(game.rank)")
+                    }
+                    .padding()
+                }
+            }
+            .onAppear(perform: fetchGames)
+        }
     }
+    
+    func fetchGames() {
+        let db = Firestore.firestore()
+
+        db.collection("gameRankings").order(by: "rank").order(by: "gameName").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                print("Successfully retrieved \(querySnapshot!.documents.count) documents.")
+                self.games = querySnapshot!.documents.compactMap { document in
+                    let data = document.data()
+                    print("Document data: \(data)") // This will print each document's data
+                    let id = document.documentID
+                    let gameName = data["gameName"] as? String ?? ""
+                    let rank = data["rank"] as? String ?? ""
+                
+                    return Game(id: id, name: gameName, rank: rank)
+                }
+            }
+        }
+    }
+
 }
 
 struct DisplayView_Previews: PreviewProvider {
